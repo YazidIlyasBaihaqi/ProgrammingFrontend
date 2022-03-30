7229d46dea98c9eefd1a61de193c79715693c69f
YazidIlyasBaihaqi <72720176+YazidIlyasBaihaqi@users.noreply.github.com>
2022-03-30 11:38:20 +0700

    Add files via upload

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,37 @@
+import Movie from "../Movie/Movie";
+import styles from "./Movies.module.css";
+import data from "../utils/constants/data.js";
+import { useState } from "react";
+
+function Movies() {
+  const [movies, setMovies] = useState(data);
+
+  function tambahFilm() {
+    const movie = {
+      id: "xyz",
+      title: "Spiral Jigsaw",
+      year: "2021",
+      type: "Movie",
+      poster: "https://picsum.photos/300/400"
+    };
+    setMovies([...movies, movie]);
+  }
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.movies}>
+        <h2 className={styles.movies__title}>Latest Movies</h2>
+        <div className={styles.movie__container}>
+        {
+          movies.map(function(movie) {
+            return <Movie key={movie.id} movie={movie}/>
+          })
+        }
+        </div>
+        <button onClick={tambahFilm}>Add Movie</button>
+      </section>
+    </div>
+  );
+}
+
+export default Movies;
